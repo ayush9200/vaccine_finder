6339e4ea1e8a0ecb15fc5cb2d05551aa038f9cb7
Ayush <ayushsingh9200@gmail.com>
2021-07-17 11:13:50 -0400

    Appointment status update

--- AdminPortal.py
+++ AdminPortal.py
@@ -4,20 +4,32 @@ import pymongo
 class AdminPortal:
 
     def authenticate(self, username, password):
-        if str(username).lower() == 'ayush' and str(password).lower() == 'admin01234':
-            return True
-        else:
-            return False
+        connection_string = "mongodb+srv://dbUser:dbUser@cluster0.w78tt.mongodb.net/Vaccine_Finder?retryWrites=true&w=majority"
+        my_client = pymongo.MongoClient(connection_string)
+        db = my_client["Vaccine_Finder"]
+        admin_cred = db["Admin"]
+        results = admin_cred.find({"$and": [{"username": username}, {"pass": password}]})
+        cred = False
+        for row in results:
+            cred = True
+        return cred
+
 
     def getAppointmentStatus(self):
         connection_string = "mongodb+srv://dbUser:dbUser@cluster0.w78tt.mongodb.net/Vaccine_Finder?retryWrites=true&w=majority"
         my_client = pymongo.MongoClient(connection_string)
         db = my_client["Vaccine_Finder"]
         vaccine = db["Appointments"]
-        ongoing = 2400
-        upcoming = 728
-        cancelled = 289
-        total = 3417
+        results = vaccine.find()
+        countForTotal = 0
+        for row in results:
+            countForTotal += 1
+            print(row)
+
+        ongoing = countForTotal - 2
+        upcoming = 2
+        cancelled = 0
+        total = countForTotal
         list_To_Return = [ongoing, upcoming, cancelled, total]
         return list_To_Return
 
