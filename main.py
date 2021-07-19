5177c7f0db30705c7b5fe5294869db5426071dcd
kulwinder31 <kulwindersingh6437@gmail.com>
2021-07-19 11:09:31 +0530

    Certificate Page updated

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, render_template
 import pymongo
 import Profile
@@ -6,13 +5,17 @@ import User
 import Appointments
 import Organisations
 import AdminPortal
+from Certificate import userFind
 from flask import Flask, session
 
 app = Flask(__name__)
 
+
 def intiate_mongoDb_conn():
-    connection_string = "mongodb+srv://dbUser:dbUser@cluster0.w78tt.mongodb.net/Vaccine_Finder?retryWrites=true&w=majority"
-    mongo_client = pymongo.MongoClient(connection_string)
+    #connection_string = "mongodb+srv://dbUser:dbUser@cluster0.w78tt.mongodb.net/Vaccine_Finder?retryWrites=true&w=majority"
+    #mongo_client = pymongo.MongoClient(connection_string)
+    connection_string = "mongodb+srv://dbUser:Qwepoi.123@cluster0.bzlp2.mongodb.net/Vaccine_Finder?retryWrites=true&w=majority";
+    my_client = pymongo.MongoClient(connection_string)
 
 
 # Started by Ayush
@@ -39,11 +42,18 @@ def admin_login():
         appointment_list_status = admin.getAppointmentStatus()
         vaccination_status = admin.getVaccinationStatus()
         if action:
-            return render_template('login-success.html', username=user_name.upper(), og_appointment=appointment_list_status[0], upg_appointment=appointment_list_status[1], cld_appointment=appointment_list_status[2], ttl_appointment=appointment_list_status[3], available_vc=vaccination_status[0], unused_vc=vaccination_status[1], expired_vc=vaccination_status[2], days_vc=vaccination_status[3])
+            return render_template('login-success.html', username=user_name.upper(),
+                                   og_appointment=appointment_list_status[0],
+                                   upg_appointment=appointment_list_status[1],
+                                   cld_appointment=appointment_list_status[2],
+                                   ttl_appointment=appointment_list_status[3], available_vc=vaccination_status[0],
+                                   unused_vc=vaccination_status[1], expired_vc=vaccination_status[2],
+                                   days_vc=vaccination_status[3])
         else:
             return render_template('login-fail.html')
     else:
         return render_template('login-fail.html')
+
 
 # Ended by Ayush
 
@@ -69,7 +79,7 @@ def register_login():
         session['email'] = email
         password = request.form.get("inputPassword")
         session['password'] = password
-        action = Appointments.insertNewUser(user_name,passport,email,password)
+        action = Appointments.insertNewUser(user_name, passport, email, password)
         if action:
             return render_template('login-success-book.html', username=user_name.upper())
         else:
@@ -97,7 +107,6 @@ def register_login():
             return render_template('user-login-fail.html')
 
 
-
 @app.route("/book-my-apptmnt", methods=['GET', 'POST'])
 def book_appointment():
     if request.method == 'POST':
@@ -108,39 +117,34 @@ def book_appointment():
         passport = session['passport']
         email = session['email']
         password = session['password']
-        return render_template('booking-success.html', username=user_name.upper(),booking_date=booking_date)
+        return render_template('booking-success.html', username=user_name.upper(), booking_date=booking_date)
     else:
         return render_template('login-fail.html')
 
 
-
-@app.route("/cert", methods= ['GET','POST'])
+@app.route("/cert", methods=['GET','POST'])
 def Certificate():
     if request.method=='GET':
         return render_template('Certificate.html')
     else:
         return render_template('homepage.html')
-@app.route('/get-certificate', methods=['GET','POST'])
+@app.route("/get-certificate", methods=['GET','POST'])
 def get_certificate():
-    if request.method == 'POST':
-        user_name = request.form.get('userName')
+    if request.method == 'POST' and request.form["btn"] == "Get Certificate":
+        print("Get Certificate button clicked")
+        user_name= request.form.get('name')
         passport = request.form.get('passport')
-        password = request.form.get('password')
-        find= Certificate.userCheck(user_name,passport,password)
-        if find:
-            return render_template('certificate-found.html')
+        password = request.form.get('inputPassword')
+        action = userFind(user_name,passport,password)
+        if action:
+            return render_template('certificate-found.html',username=user_name.upper() )
         else:
             return render_template('user-login-fail.html')
+
 
 @app.route('/about')
 def about_us():
     return render_template('FAQ.html')
-   
-    
-
-
-
-
 
 
 if __name__ == "__main__":
